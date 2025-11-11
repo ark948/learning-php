b0fda9e45e1070aaf04568a3b1a55b08a2eca2c9
ark948 <armankarimi948@gmail.com>
2025-11-11 21:20:16 +0330

    2.2 creating new tables

---
+++ php_pdo/2.2_creating_new_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+// to create new table, we use the CREATE TABLE statement
+
+// we can execute sql statements using the exec() method of PDO instnace
+// it returns the number of affected rows on success, or false on failure
+
+$statements = [
+	'CREATE TABLE authors( 
+        author_id   INT AUTO_INCREMENT,
+        first_name  VARCHAR(100) NOT NULL, 
+        middle_name VARCHAR(50) NULL, 
+        last_name   VARCHAR(100) NULL,
+        PRIMARY KEY(author_id)
+    );',
+	'CREATE TABLE book_authors (
+        book_id   INT NOT NULL, 
+        author_id INT NOT NULL, 
+        PRIMARY KEY(book_id, author_id), 
+        CONSTRAINT fk_book 
+            FOREIGN KEY(book_id) 
+            REFERENCES books(book_id) 
+            ON DELETE CASCADE, 
+            CONSTRAINT fk_author 
+                FOREIGN KEY(author_id) 
+                REFERENCES authors(author_id) 
+                ON DELETE CASCADE
+    )'
+];
+
+
+$pdo = require 'connect.php';
+
+// execute sql statements (there are two of them)
+foreach ($statements as $statement) {
+    $pdo->exec($statement);
+}
