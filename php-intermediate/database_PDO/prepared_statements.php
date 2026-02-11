dd2c989a06288646ad3bfccec6f193a7b7894282
ark948 <armankarimi948@gmail.com>
2026-02-11 11:52:25 +0330

    php intermediate, PDO, prepared statements

---
+++ php-intermediate/database_PDO/prepared_statements.php
@@ -0,0 +1,32 @@
+<?php
+
+
+require "connection.php";
+
+// $insert = $conn->query(
+//     "INSERT INTO posts (title, body) 
+//             VALUES ('post three', 'body three')"
+// );
+
+
+// in some cases, data to insert are not known, or are to be aquired later
+// in those cases we use prepared statements
+
+$title = "title 4";
+$body = "body of post 4";
+
+$insert = $conn->prepare(
+    "INSERT INTO posts (title, body) VALUES (:title, :body)"
+);
+
+// now bind the handlers (or execute and bind at once)
+$insert->execute(array(
+    ':title' => $title,
+    ':body' => $body
+));
+
+
+// a similar syntax is to ? in place of handlers
+// then we no longer need to provide index names
+// query: "INSERT INTO posts (title, body) VALUES (?, ?)"
+// $insert->execute(array($title, $body));
