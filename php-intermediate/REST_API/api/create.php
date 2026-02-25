e35988af934ee7fc9dc33841201b4d12ed905f6e
ark948 <armankarimi948@gmail.com>
2026-02-25 14:47:31 +0330

    php intermediate, REST API project, Creating posts

---
+++ php-intermediate/REST_API/api/create.php
@@ -0,0 +1,39 @@
+<?php
+
+require "../autoload.php";
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST");
+// Access-Control-Allow-Methods: Specifies one or more HTTP request methods allowed when accessing a resource
+// Access-Control-Allow-Methods: POST indicates that this endpoint accepts POST method
+
+// by default, browsers send a preflight request (OPTIONS request) to check what is allowed.
+// if you're backend only accepts POST, you must declare it. Otherwise the browser will block the frontend call.
+
+// Since frontend may be on another domain (CORS needs to be configured properly)
+
+use App\Controllers\PostsController;
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $createNewPost = new PostsController();
+    $post = json_decode(json: file_get_contents(filename: "php://input"));
+    $data = [
+        "title" => $post->title,
+        "body" => $post->body,
+        "author" => $post->author
+    ];
+
+    if ($createNewPost->store($data)): // if the return value was true...
+        http_response_code(201);
+        echo json_encode([
+            "status" => true,
+            "message" => "Post was created. OK",
+            "data" => $data
+        ]);
+    else:
+        echo json_encode([
+            "status" => false,
+            "message" => "error, Post was not created",
+        ]);
+    endif;
+}
--- php-intermediate/REST_API/src/Controllers/PostsController.php
+++ php-intermediate/REST_API/src/Controllers/PostsController.php
@@ -6,9 +6,9 @@ use App\Models\Post;
 class PostsController
 {
     public function store($data) {
-        $title = trim(htmlspecialchars($_POST['title']));
-        $body = trim(htmlspecialchars($_POST['body']));
-        $author = trim(htmlspecialchars($_POST['author']));
+        $title = trim(htmlspecialchars($data['title']));
+        $body = trim(htmlspecialchars($data['body']));
+        $author = trim(htmlspecialchars($data['author']));
 
         if ($title AND $body AND $author) {
             $inserted = Post::createPost($title, $body, $author);
