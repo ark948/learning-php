07d8633e7e03672ca83bb6f84d4c86612040fed0
ark948 <armankarimi948@gmail.com>
2026-02-25 21:57:04 +0330

    php intermediate, REST API project, delete

---
+++ php-intermediate/REST_API/api/delete.php
@@ -0,0 +1,38 @@
+<?php
+
+require "../autoload.php";
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: DELETE");
+
+use App\Models\Post;
+
+$id = filter_input(type: INPUT_GET, var_name: "id", filter: FILTER_VALIDATE_INT);
+
+if (!$id) {
+    echo json_encode([
+        "status" => false, 
+        "message" => "there was an error."
+    ]);
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    $post = Post::getSinglePost($id);
+    if ($post) {
+        $deleted = Post::deletePost($id);
+        if ($deleted):
+            http_response_code(200);
+            echo json_encode([
+                "status" => true,
+                "message" => "Post deleted successfully",
+            ]);
+        endif;
+    } else {
+        echo json_encode([
+            "status" => false,
+            "message" => "No posts with given id."
+        ]);
+    }
+}
+
