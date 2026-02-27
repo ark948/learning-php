0847984a31231c32ec8da9d12c62094e524fbd45
ark948 <armankarimi948@gmail.com>
2026-02-27 18:03:15 +0330

    php intermediate, Laravel basics, creating migrations

--- php-intermediate/frameworks_intro/backend/app/Models/Post.php
+++ php-intermediate/frameworks_intro/backend/app/Models/Post.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = "posts"; // for demonstration purposes
+    public $timestamps = true; // automatically manage created_at and updated_at fields
 }
